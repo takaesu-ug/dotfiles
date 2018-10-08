79c5df778c4d026853cf5c4df845ca80fc8625a3
Yuji Takaesu <taka.esu.user@gmail.com>
2018-10-08 10:27:53 +0900

    change padding size of hyper

--- .hyper.js
+++ .hyper.js
@@ -62,7 +62,7 @@ module.exports = {
     showWindowControls: '',
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: '2px 4px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
