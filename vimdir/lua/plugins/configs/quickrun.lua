6439afed7bb030335c05ac73db468867aedb535c
Yuji Takaesu <taka.esu.user@gmail.com>
2025-07-22 06:55:44 +0900

    Remove plugins

--- vimdir/lua/plugins/configs/quickrun.lua
+++
@@ -1,7 +0,0 @@
--- plugins/configs/quickrun.lua
-
--- quickrun初期化
-vim.g.quickrun_config = {}
-
--- キーバインド設定
-vim.keymap.set('n', '<Leader>rr', '<plug>(quickrun)', {})
--- vimdir/lua/plugins/init.lua
+++ vimdir/lua/plugins/init.lua
@@ -27,31 +27,26 @@ local plugins = {
   },
 
   -- 基本プラグイン
-  { 'tpope/vim-surround' },
-  { 'h1mesuke/vim-alignta' },
   {
-    'rhysd/accelerated-jk',
-    config = function()
-      require('plugins.configs.accelerated-jk')
-    end,
-  },
-  { 'tpope/vim-endwise' },
-
-  -- FZF
-  {
-    'junegunn/fzf',
-    build = function()
-      vim.fn['fzf#install']()
-    end,
-  },
-  {
-    'junegunn/fzf.vim',
-    dependencies = { 'junegunn/fzf' },
-    config = function()
-      require('plugins.configs.fzf')
-    end,
+    'tpope/vim-surround',
+    event = 'VeryLazy',
   },
 
+--  -- FZF
+--  {
+--      'junegunn/fzf',
+--      build = function()
+--          vim.fn['fzf#install']()
+--      end,
+--  },
+--  {
+--      'junegunn/fzf.vim',
+--      dependencies = { 'junegunn/fzf' },
+--      config = function()
+--          require('plugins.configs.fzf')
+--      end,
+--  },
+--
   -- 括弧補完
   {
     'kana/vim-smartinput',
@@ -64,69 +59,55 @@ local plugins = {
     event = 'VeryLazy',
   },
 
-  -- キャメルケース移動
-  {
-    'bkad/CamelCaseMotion',
-    config = function()
-      require('plugins.configs.camelcasemotion')
-    end,
-  },
-
+--  -- キャメルケース移動
+--  {
+--      'bkad/CamelCaseMotion',
+--      config = function()
+--          require('plugins.configs.camelcasemotion')
+--      end,
+--  },
+--
   -- Git
   {
-    'tpope/vim-fugitive',
-    cmd = { 'Git', 'Gstatus', 'Gblame', 'Gpush', 'Gpull' },
+      'tpope/vim-fugitive',
+      cmd = { 'Git', 'Gstatus', 'Gblame', 'Gpush', 'Gpull' },
   },
 
-  -- ブラウザ連携
-  {
-    'tyru/open-browser.vim',
-    config = function()
-      require('plugins.configs.open-browser')
-    end,
-  },
-
-  -- コメント
-  {
-    'tomtom/tcomment_vim',
-    config = function()
-      require('plugins.configs.tcomment')
-    end,
-  },
-
-  -- 実行
-  {
-    'thinca/vim-quickrun',
-    config = function()
-      require('plugins.configs.quickrun')
-    end,
-  },
-
-  -- 検索置換サポート
-  { 'tpope/vim-abolish' },
-
-  -- リンター
-  {
-    'w0rp/ale',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      require('plugins.configs.ale')
-    end,
-  },
-
-  -- LSP（COC）
-  {
-    'neoclide/coc.nvim',
-    branch = 'release',
-    event = 'VeryLazy',
-    config = function()
-      require('plugins.configs.coc')
-    end,
-  },
+--  -- コメント
+--  {
+--      'tomtom/tcomment_vim',
+--      config = function()
+--          require('plugins.configs.tcomment')
+--      end,
+--  },
+--
+--  -- 検索置換サポート
+--  { 'tpope/vim-abolish' },
+--
+--  -- リンター
+--  {
+--      'w0rp/ale',
+--      event = { 'BufReadPre', 'BufNewFile' },
+--      config = function()
+--          require('plugins.configs.ale')
+--      end,
+--  },
+--
+--  -- LSP（COC）
+--  {
+--      'neoclide/coc.nvim',
+--      branch = 'release',
+--      event = 'VeryLazy',
+--      config = function()
+--          require('plugins.configs.coc')
+--      end,
+--  },
 
   -- ステータスライン
   {
     'itchyny/lightline.vim',
+    lazy = false,
+    priority = 1000,
     config = function()
       require('plugins.configs.lightline')
     end,
@@ -135,42 +116,17 @@ local plugins = {
   -- インデントガイド
   {
     'nathanaelkane/vim-indent-guides',
-    event = 'VeryLazy',
+    event = "VimEnter",
     config = function()
       require('plugins.configs.indent-guides')
     end,
   },
 
-  -- カラープレビュー
-  {
-    'lilydjwg/colorizer',
-    cmd = { 'ColorHighlight', 'ColorClear', 'ColorToggle' },
-  },
-
-  -- 言語別プラグイン
-  {
-    'tpope/vim-rails',
-    ft = 'ruby',
-  },
-
-  -- その他
-  { 'editorconfig/editorconfig-vim' },
-  {
-    'cespare/vim-toml',
-    ft = 'toml',
-  },
-  {
-    'kannokanno/previm',
-    ft = 'markdown',
-    dependencies = { 'tyru/open-browser.vim' },
-  },
-  {
-    'hashivim/vim-terraform',
-    ft = 'terraform',
-    config = function()
-      require('plugins.configs.terraform')
-    end,
-  },
+--  -- カラープレビュー
+--  {
+--      'lilydjwg/colorizer',
+--      cmd = { 'ColorHighlight', 'ColorClear', 'ColorToggle' },
+--  },
 }
 
 -- lazy.nvimのセットアップ
@@ -184,7 +140,7 @@ require('lazy').setup(plugins, {
         'gzip',
         'matchit',
         'matchparen',
-        'netrwPlugin',
+--        'netrwPlugin',
         'tarPlugin',
         'tohtml',
         'tutor',
