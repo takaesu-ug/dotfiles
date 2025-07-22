73fda831e161c7e62e9cbd94311feb0aa95b59e2
Yuji Takaesu <taka.esu.user@gmail.com>
2025-07-22 09:50:07 +0900

    Remove plugin settings

--- vimdir/lua/plugins/configs/open-browser.lua
+++
@@ -1,8 +0,0 @@
--- plugins/configs/open-browser.lua
-
--- URLをブラウザで開く
-vim.keymap.set('n', '<Leader>o', '<Plug>(openbrowser-open)', {})
-vim.keymap.set('v', '<Leader>o', '<Plug>(openbrowser-open)', {})
-
--- ググる
-vim.keymap.set('n', '<Leader>g', ':<C-u>OpenBrowserSearch<Space><C-r><C-w><Enter>', {})
