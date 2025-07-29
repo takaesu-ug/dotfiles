28e8c8a823a5f2c45cd0e5120b8269f6ce6698c2
Yuji Takaesu <taka.esu.user@gmail.com>
2025-07-29 13:46:36 +0900

    Fix gitignore

---
+++ vimdir/ftdetect/sqlx.lua
@@ -0,0 +1,13 @@
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*.sqlx",
+  callback = function()
+    vim.bo.filetype = "sql"
+    vim.bo.commentstring = "-- %s"
+
+    -- BigQuery固有のキーワードハイライト（オプション）
+    vim.cmd([[
+      syntax keyword sqlKeyword STRUCT ARRAY UNNEST SAFE_CAST
+      syntax keyword sqlFunction CURRENT_DATETIME PARSE_DATETIME
+    ]])
+  end,
+})
