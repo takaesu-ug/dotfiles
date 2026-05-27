7a2ae7f13c1c89390c7e059fec04533dc67bb0fa
Yuji Takaesu <taka.esu.user@gmail.com>
2026-05-27 15:29:54 +0900

    Fix nvim-treesitter

--- vimdir/lua/plugins/configs/treesitter.lua
+++ vimdir/lua/plugins/configs/treesitter.lua
@@ -1,31 +1,41 @@
 -- lua/plugins/configs/treesitter.lua
+-- nvim-treesitter main ブランチ (Neovim 0.12+ 用の書き直し版)
+-- master ブランチはアーカイブ予定のため移行
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
   lazy = false,
   priority = 1000,
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "diff",
-        "lua",
-        "ruby",
-        "javascript",
-        "python",
-        "vim",
-        "vimdoc",
-        "html",
-        "markdown",
-        "sql",
-        "bash",
-        "typescript",
-        "tsx",
-        "yaml",
-      },
-      auto_install = true,
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
+    local ensure_installed = {
+      "diff",
+      "lua",
+      "ruby",
+      "javascript",
+      "python",
+      "vim",
+      "vimdoc",
+      "html",
+      "markdown",
+      "markdown_inline",
+      "sql",
+      "bash",
+      "typescript",
+      "tsx",
+      "yaml",
+    }
+
+    require("nvim-treesitter").install(ensure_installed)
+
+    -- ハイライトを有効化
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        local ok = pcall(vim.treesitter.start, args.buf)
+        if not ok then return end
+        -- インデント (experimental)
+        vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
